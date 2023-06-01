1dbd0ef367b2acc929d0c547cedb579119b9cee3
Oğuzhan Kahraman <oguzhankahramann@gmail.com>
2023-06-01 11:24:50 +0300

    test.py

--- test.py
+++ test.py
@@ -11,60 +11,48 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
-# Giriş bilgilerini tanımlayın
-kullanici_adi = "username"
-sifre = "password"
+# Define login credentials
+username = "username"
+password = "password"
 
-# ChromeDriver'ın dosya yolunu ve seçeneklerini belirtin
-driver_path = (
-    "C:/Python311/chromedriver.exe"  # ChromeDriver'ın dosya yolunu buraya girin
-)
+# Specify the file path and options for ChromeDriver
+driver_path = "C:/Python311/chromedriver.exe"  # Enter the file path of ChromeDriver here
 options = webdriver.ChromeOptions()
-options.add_argument("--headless")  # Arka planda çalışmasını sağlar (isteğe bağlı)
+options.add_argument("--headless")  # Enables running in headless mode (optional)
 
-# WebDriver'ı yapılandırın
+# Configure the WebDriver
 driver = webdriver.Chrome(executable_path=driver_path, options=options)
 
-# Panele giriş yapın
+# Log in to the panel
 driver.get("https://ilanbis.bik.gov.tr")
-driver.find_element(By.CSS_SELECTOR, "input#UserName.form-control").send_keys(
-    kullanici_adi
-)
-driver.find_element(By.CSS_SELECTOR, "input#Password.form-control").send_keys(sifre)
-driver.find_element(
-    By.CSS_SELECTOR, "button#giris.btn.btn-primary.btn-flat.btn-block"
-).click()
+driver.find_element(By.CSS_SELECTOR, "input#UserName.form-control").send_keys(username)
+driver.find_element(By.CSS_SELECTOR, "input#Password.form-control").send_keys(password)
+driver.find_element(By.CSS_SELECTOR, "button#giris.btn.btn-primary.btn-flat.btn-block").click()
 
-# Bekleyin ve ilan sayfasına gidin
+# Wait and go to the announcement page
 wait = WebDriverWait(driver, 5)
-ilan_linki = "https://ilanbis.bik.gov.tr/Ilan/IndexYayin"
-driver.get(ilan_linki)
+announcement_link = "https://ilanbis.bik.gov.tr/Ilan/IndexYayin"
+driver.get(announcement_link)
 
-# Bekleyin ve checkbox'ı seçin
-checkbox = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "icheckbox_square-blue"))
-)
+# Wait and select the checkbox
+checkbox = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "icheckbox_square-blue")))
 checkbox.click()
 
-# Bekleyin ve ilanlar butonuna basın
-ilanlar_butonu = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "btn-group.ilanlar-buttons"))
-)
-ilanlar_butonu.click()
+# Wait and click on the "ilanlar" button
+announcements_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "btn-group.ilanlar-buttons")))
+announcements_button.click()
 
-# Bekleyin ve ilanları indirin
-time.sleep(2)  # İlanların yüklenmesi için bir süre bekleyin
+# Wait and download the announcements
+time.sleep(2)  # Wait for the announcements to load
 
-# İndirme klasörünü belirleyin
-download_folder = (
-    "C:/Users/your_account/Downloads"  # İndirme klasörünü uygun şekilde değiştirin
-)
+# Specify the download folder
+download_folder = "C:/Users/your_account/Downloads"  # Modify the download folder accordingly
 
-# Bekleyin ve en son indirilen dosyayı bulun
+# Wait and find the latest downloaded file
 latest_file = None
 start_time = time.time()
 
-while time.time() - start_time < 10:  # İndirilen dosyayı 10 saniye boyunca bekleyin
+while time.time() - start_time < 10:  # Wait for the downloaded file for 10 seconds
     time.sleep(1)
     files = glob.glob(download_folder + "/*")
     if files:
@@ -72,49 +60,48 @@ while time.time() - start_time < 10:  # İndirilen dosyayı 10 saniye boyunca be
         break
 
 if latest_file is not None:
-    print("En son indirilen dosya:", latest_file)
-    dosya_adi = os.path.basename(latest_file)
-    indirilen_dosya_yolu = latest_file
+    print("Latest downloaded file:", latest_file)
+    file_name = os.path.basename(latest_file)
+    downloaded_file_path = latest_file
 
-    # E-posta ayarları
-    gonderen_email = "send_mail"
-    alicilar = [
+    # Email settings
+    sender_email = "send_mail"
+    recipients = [
         "mail_1",
         "mail_2",
         "mail_3",
     ]
-    gonderen_sifre = "mail_password*"
-    mail_sunucusu = "smtp"
-    mail_portu = 465
+    sender_password = "mail_password*"
+    mail_server = "smtp"
+    mail_port = 465
 
-    # E-posta gönderme işlemi
+    # Email sending process
     msg = MIMEMultipart()
-    msg["From"] = gonderen_email
-    msg["To"] = ", ".join(alicilar)
+    msg["From"] = sender_email
+    msg["To"] = ", ".join(recipients)
     msg["Subject"] = "subject"
 
-    # E-posta metnini ekleyin (isteğe bağlı)
-    mesaj_metni = "text"
-    msg.attach(MIMEText(mesaj_metni, "plain"))
+    # Add email body (optional)
+    message_text = "text"
+    msg.attach(MIMEText(message_text, "plain"))
 
-    # İndirilen dosyayı ekleyin
-    with open(indirilen_dosya_yolu, "rb") as dosya:
-        icerik_ek = MIMEApplication(dosya.read(), Name=dosya_adi)
-    icerik_ek["Content-Disposition"] = f"attachment; filename={dosya_adi}"
-    msg.attach(icerik_ek)
+    # Add the downloaded file as an attachment
+    with open(downloaded_file_path, "rb") as file:
+        attachment = MIMEApplication(file.read(), Name=file_name)
+    attachment["Content-Disposition"] = f"attachment; filename={file_name}"
+    msg.attach(attachment)
 
-    # E-posta sunucusuna bağlanın ve e-postayı gönderin
+    # Connect to the email server and send the email
     try:
-        server = smtplib.SMTP_SSL(mail_sunucusu, mail_portu)
-        server.login(gonderen_email, gonderen_sifre)
+        server = smtplib.SMTP_SSL(mail_server, mail_port)
+        server.login(sender_email, sender_password)
         server.send_message(msg)
         server.quit()
-        print("E-posta gönderildi.")
+        print("Email sent.")
     except Exception as e:
-        print("E-posta gönderme hatası:", str(e))
+        print("Email sending error:", str(e))
 else:
-    print("İndirilen dosya bulunamadı.")
+    print("Downloaded file not found.")
 
-
-# WebDriver'ı kapatın
+# Close the WebDriver
 driver.quit()
